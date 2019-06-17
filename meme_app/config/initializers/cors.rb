6bd60c29d39eab8daeb082c7edca9eae61609abe
Superunknown1103 <superunknown1103@gmail.com>
2019-06-17 11:51:52 -0500

    cors issues solved

---
+++ meme_app/config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:3001'
+      
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  end
